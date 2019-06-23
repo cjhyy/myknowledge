28692edf49455b50d12c70259f31be43701eb6ec
maki maki <chenjunhong54321@163.com>
2019-06-23 17:45:08 +0800

    啊

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,53 @@
+module.exports = {
+    title: '知识',
+    description: '春江花月夜的知识仓库',
+    base: '/Prebook/',
+    head: [
+        ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
+    ],
+    markdown: {
+        lineNumbers: true // 代码块显示行号
+    },
+    themeConfig: {
+
+        lastUpdated: '最后更新时间',
+        // 2.设置true，开启最后更新时间
+        lastUpdated: true,
+        nav: [
+            { text: '前端', link: 'https://cjhyy.github.io/Prebook/' },
+            {
+                text: '算法',
+                items: [
+                    { text: '算法与数据结构', link: 'https://github.com/cjhyy' },
+                    { text: 'leetcode题解', link: 'https://cjhyy.github.io/myleetcode/' }
+                ]
+            },
+
+            {
+                text: '学习笔记',
+                items: [
+                    { text: '数学', link: 'https://github.com/cjhyy' },
+                    {
+                        text: '计算机',
+                        link: 'https://github.com/cjhyy'
+                    },
+                    {
+                        text: '英语',
+                        link: 'https://github.com/cjhyy'
+                    },
+                    {
+                        text: '日语',
+                        link: 'https://github.com/cjhyy'
+                    },
+                    {
+                        text: '其他',
+                        link: 'https://github.com/cjhyy'
+                    },
+                ]
+            },
+            { text: 'Github', link: 'https://github.com/cjhyy' },
+        ]
+
+
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+lang: zh-CN
+heroText: cjhyy的知识仓库
+heroImage: /logo.jpg
+actionText: 开始
+actionLink: /
+features:
+- title: 
+  details: 江畔何人初见月，江月何年初照人。
+- title: 
+  details: -张若虚
+footer: Copyright © 2019-present cjhyy
+---
